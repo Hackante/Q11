fa9e7f7e5081894c982f25c5ec9e8af99212da2c
Hackante <kante.hagen@gmail.com>
2023-01-22 19:00:25 +0100

    Rekursive Methoden

--- Playlist/KNOTEN.java
+++ Playlist/KNOTEN.java
@@ -1,20 +1,80 @@
 public class KNOTEN {
-    private SONG song;
+    private DATENELEMENT daten;
     private KNOTEN nachfolger;
 
-    public KNOTEN(SONG song) {
-        this.song = song;
+    public KNOTEN(DATENELEMENT d) {
+        daten = d;
+        nachfolger = null;
+    }
+
+    // Getter- und Setter-Methoden
+    public void setNachfolger(KNOTEN k) {
+        nachfolger = k;
     }
 
     public KNOTEN getNachfolger() {
-        return this.nachfolger;
+        return nachfolger;
     }
 
-    public void setNachfolger(KNOTEN nachfolger) {
-        this.nachfolger = nachfolger;
+    public void setDaten(DATENELEMENT d) {
+        daten = d;
     }
 
-    public SONG getSong() {
-        return this.song;
+    public DATENELEMENT getDaten() {
+        return daten;
+    }
+
+    public void ausgeben() {
+        daten.ausgeben();
+    }
+
+    public int getRestlänge() {
+        if (nachfolger == null) {
+            return 1;
+        } else {
+            return nachfolger.getRestlänge() + 1;
+        }
+    }
+
+    public double getRestdauer() {
+        if (nachfolger == null) {
+            return daten.getDauer();
+        } else {
+            return nachfolger.getRestdauer() + daten.getDauer();
+        }
+    }
+
+    public double getRestbewertung() {
+        if (nachfolger == null) {
+            return daten.getBewertung();
+        } else {
+            return nachfolger.getRestbewertung() + daten.getBewertung();
+        }
+    }
+
+    public double getRestjahr() {
+        if (nachfolger == null) {
+            return daten.getJahr();
+        } else {
+            return nachfolger.getRestjahr() + daten.getJahr();
+        }
+    }
+
+    public double getRestpreis() {
+        if (nachfolger == null) {
+            return daten.getPreis();
+        } else {
+            return nachfolger.getRestpreis() + daten.getPreis();
+        }
+    }
+
+    public void restlicheInfosAusgeben() {
+        if (nachfolger == null) {
+            daten.ausgeben();
+        } else {
+            daten.ausgeben();
+            System.out.println();
+            nachfolger.restlicheInfosAusgeben();
+        }
     }
 }
